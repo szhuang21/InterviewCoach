0b4cce78f01aa3135f7b2c167b14cf241101bf7c
Kelsi Padalia <kelsip2@illinois.edu>
2023-06-17 15:13:00 -0700

    Create constants file

---
+++ backend/constants.py
@@ -0,0 +1,11 @@
+positive_emotions_language = ["Admiration", "Adoration", "Aesthetic Appreciation", "Amusement", "Awe", "Calmness", "Concentration", "Contemplation", "Contentment", "Craving", "Desire",
+                              "Determination", "Ecstasy", "Empathic Pain", "Enthusiasm", "Entrancement", "Excitement", "Gratitude", "Interest", "Joy", "Love", "Nostalgia", 
+                              "Pride", "Realization", "Relief", "Romance", "Satisfaction", "Surprise (positive)", "Sympathy", "Triumph"]
+positive_emotions_other = ["Admiration", "Adoration", "Aesthetic Appreciation", "Amusement", "Awe", "Calmness", "Concentration", "Contemplation", "Contentment", "Craving", "Desire",
+                           "Determination", "Empathic Pain", "Enthusiasm", "Entrancement", "Excitement", "Interest", "Joy", "Love", "Nostalgia", "Pride", "Realization", 
+                           "Relief", "Romance", "Satisfaction", "Surprise (positive)", "Sympathy", "Triumph"]
+
+negative_emotions_language = ["Anger", "Annoyance", "Anxiety", "Awkwardness", "Boredom", "Confusion", "Contempt", "Disappointment", "Disapproval", "Disgust", "Distress", "Doubt",
+                              "Embarrassment", "Envy", "Fear", "Guilt", "Horror", "Pain", "Sadness", "Sarcasm", "Shame", "Surprise (negative)", "Tiredness"]
+negative_emotions_other = ["Anger", "Anxiety", "Awkwardness", "Boredom", "Confusion", "Disappointment", "Disgust", "Distress", "Doubt", "Embarrassment", "Envy", "Fear", "Guilt", 
+                           "Horror", "Pain", "Sadness", "Shame", "Surprise (negative)", "Tiredness"]
