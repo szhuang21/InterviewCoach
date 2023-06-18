431f78d80399b7b321630c6f11678914100cf490
Prachi Deo <prachisdeo@berkeley.edu>
2023-06-17 17:13:55 -0700

    testing videos

--- backend/report.py
+++ backend/report.py
@@ -1,14 +1,20 @@
 from hume import HumeBatchClient
 from hume.models.config import FaceConfig
+from hume.models.config import LanguageConfig
+from hume.models.config import BurstConfig
+import os
 
 client = HumeBatchClient("HYNdEFrlFnYpEUJAcUfaKYf8Or8qMyIo3IFzuAQBlcFGiE24")
-urls = ["https://hume-tutorials.s3.amazonaws.com/faces.zip"]
-config = FaceConfig()
-job = client.submit_job(urls, [config])
+cwd = os.getcwd()
+#urls =
+files = ["/Users/prachideo/Desktop/AI_Hackathon/InterviewCoach/backend/test_videos/vocal_vid.mov"] #["https://hume-tutorials.s3.amazonaws.com/faces.zip"]
+config = LanguageConfig()
+job = client.submit_job(None, [config], files=files)
+
 
 print(job)
 print("Running...")
 
 details = job.await_complete()
-job.download_predictions("predictions.json")
-print("Predictions downloaded to predictions.json")
+job.download_predictions("test_lang_predictions.json")
+print("Predictions downloaded to test_lang_predictions.json")
